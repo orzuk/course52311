1bc0b407b7c73c818b79f700d1435cd1e5f72de3
orzuk <oorzuk@gmail.com>
2015-06-21 16:04:56 +0300

    Formatted video data

--- src/Read_Video_Dataset.m
+++ src/Read_Video_Dataset.m
@@ -3,5 +3,16 @@
 
 data_dir = 'C:/Users/user/Documents/GitHub/course52311/data'; 
 
-V = videoreader(fullfile(data_dir, 'Video/Short video clip-nature.mp4')); 
+V = VideoReader(fullfile(data_dir, 'Video/Short video clip-nature.mp4')); 
+
+frame_width = V.Width;
+frame_length = V.Height; 
+num_frames = V.numberOfFrames;
+VideoData = V.read; 
+save(fullfile(data_dir, 'Video/Short video clip-nature.mp4'), 'VideoData', 'num_frames', 'frame_width', 'frame_length'); 
+
+num_pixel 
+
+CC = VideoReader(fullfile(data_dir, 'Video/CC_1916_10_02_ThePawnshop_512kb.mp4')); 
+
 
